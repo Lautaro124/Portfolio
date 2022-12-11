8adfc5865d092b9fd7fb54ef1751557be512598f
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2022-12-11 03:34:39 -0300

    Fac icon and logo

--- src/components/NavBar/NabBar.tsx
+++ src/components/NavBar/NabBar.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import DesktopNavSection from '../DesktopNavSection/DesktopNavSection'
 import MobileNavSection from '../MobileNavSection/MobileNavSection'
+import Logo from '../../assets/Logo.png'
 
 const NabBar = () => {
   const [nav, setNav] = useState<boolean>(false)
 
   return (
     <div className='flex justify-between items-center w-full px-4 h-20 text-white fixed bg-black'>
-      <h1 className="text-5xl font-signature ml-2">
-        La
-      </h1>
+      <img
+        className='w-14'
+        src={Logo} 
+        alt='No logo' 
+      />
       <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
         {
           nav ?
